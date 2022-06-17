946a748037ff6f1424840b49d230a51fc3c1a62c
Saurabh97Python <107696673+Saurabh97Python@users.noreply.github.com>
2022-06-17 19:44:06 +0530

    Add files via upload

---
+++ python loan approval project.py
@@ -0,0 +1,11 @@
+
+good_credit_score= True
+good_bank_balance= True
+
+if good_credit_score and good_bank_balance:
+    print('Loan approved....')
+
+
+else:
+    print('Sorry ..not eligible for loan')
+
